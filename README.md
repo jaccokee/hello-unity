2eea928c0654ce89b7f071724920f05247bc3032
John Kee <jkee@rosettastone.com>
2014-12-08 08:23:06 -0600

    HW1 Complete

---
+++ README.md
@@ -0,0 +1,9 @@
+### First Unity homework assignment ###
+
+## Objective: ##
+
+* Create a scene
+* Create any object, such as a cube
+* Control that object (with keyboard or whatever input device)
+* Add lighting
+
---
+++ hello-unity/Assets/Standard Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {}
+
+	// Update is called once per frame
+	void Update () {
+		// Get the input vector from keyboard or analog stick
+		var directionVector = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+		rigidbody.velocity = directionVector * speed;
+	}
+}
